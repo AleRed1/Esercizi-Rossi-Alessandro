f326b4de7721f4fe163b26a13635ec1a7b86d862
Alessandro Rossi <33749887+AleRed1@users.noreply.github.com>
2018-05-04 12:31:26 +0200

    Devo aggiungere i commenti su tutti

--- Esercizio27.ps1
+++
@@ -1,6 +0,0 @@
-# Esercizio 27
-
-if($args -gt 10 && $args != null)    # se il parametro passato è maggiore a 10 
-	echo > di 10		     #scrive > 10
-else
-	echo < o = a 10              #scrive < o = a 10

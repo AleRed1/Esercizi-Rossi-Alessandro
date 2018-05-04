840861c1c6ed5938c8adac4eba03fe27c4290017
Alessandro Rossi <33749887+AleRed1@users.noreply.github.com>
2018-05-04 12:31:55 +0200

    Esercizio 18

---
+++ Esercizio18.ps1
@@ -0,0 +1,5 @@
+#Esercizio 19 Rossi Alessandro
+
+new-item "Escape.txt" -itemtype file #Per creare il file "Escape.txt"
+
+get-help about_escape_character | tee "Escape.txt" #Scrive sul file "Escape.txt" il risultato del comando precedente
